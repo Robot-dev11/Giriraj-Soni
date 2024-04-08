2b853c178a99ada17ecc6181686ad7d14a1f8992
Robot-dev11 <girirajsoni1999@gmail.com>
2024-04-08 20:39:51 +0530

    experience page added

---
+++ src/experience/main.11ty.js
@@ -0,0 +1,17 @@
+exports.data = () => {
+    return {
+        layout: "base.njk",
+        permalink: "/experience",
+    }
+}
+exports.render = (data) =>{
+    return `
+        ${data.collections.experience.sort((a, b) => {
+            return a.data.postion - b.data.position
+        }).map(
+            post => {
+                return post.content;
+            }
+        ).join("\n")}
+    `
+}
