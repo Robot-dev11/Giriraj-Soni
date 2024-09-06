0694e4a208d1fc9d9d1c3c7380f2228fce106289
Giriraj Soni <girirajsoni1999@gmail.com>
2024-09-06 15:19:26 +0530

    Email Notification Setup Done

--- src/contact/contact.11ty.js
+++ src/contact/contact.11ty.js
@@ -1,7 +1,7 @@
 const phoneList = require('../_data/iso-code');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-
+const contactAPI = process.env.CONTACT_FORM_API
 
 
 class Contact {
@@ -29,15 +29,15 @@ class Contact {
       <form id="contactus-inquiry" class="mx-auto mt-16 max-w-xl sm:mt-20">
         <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+            <label for="firstName" class="block text-sm font-semibold leading-6 text-gray-900">First name*</label>
             <div class="mt-2.5">
-              <input required type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+              <input required type="text" name="firstName" id="firstName" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             </div>
           </div>
         <div>
-          <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+          <label for="lastName" class="block text-sm font-semibold leading-6 text-gray-900">Last name*</label>
           <div class="mt-2.5">
-            <input required type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input required type="text" name="lastName" id="lastName" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
         <div class="sm:col-span-2">
@@ -47,28 +47,13 @@ class Contact {
           </div>
         </div>
         <div class="sm:col-span-2">
-          <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+          <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email*</label>
           <div class="mt-2.5">
             <input required type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
         <div class="sm:col-span-2">
-          <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
-          <div class="relative mt-2.5">
-            <div class="absolute inset-y-0 left-0 flex items-center">
-              <label for="country" class="sr-only">Country</label>
-              <select id="country" name="country" class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                ${phoneList.map(country => `<option>${country.code}</option>`)}
-              </select>
-              <svg class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-              </svg>
-            </div>
-            <input required type="tel" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-        <div class="sm:col-span-2">
-          <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+          <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message*</label>
           <div class="mt-2.5">
             <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
           </div>
@@ -93,16 +78,31 @@ function submitInquiry(formData){
     <script>
       document.getElementById('${formData}').addEventListener('submit', async function(event){
         event.preventDefault();
-        console.log(event);
-        console.log(event.target);
 
         const responseDiv = document.getElementById('${formData}-response');
         if(responseDiv) responseDiv.innerHTML = "";
 
         const data = new FormData(event.target);
         const inquiryInfo = Object.fromEntries(data.entries());
-        console.log('i: ', inquiryInfo)
-        return inquiryInfo;
+        console.log("i: ", inquiryInfo)
+    
+        const res = await fetch('${contactAPI}', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(inquiryInfo)
+        })
+
+        if (responseDiv) {
+          responseDiv.innerHTML = "${resDivText}";
+        }
+
+        event.target.reset();
+
+        setTimeout(() => {
+        responseDiv.innerHTML = "";
+        }, 5000)
       })
 
     </script>
